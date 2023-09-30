ee7b5fa3ee9e5ed343961a4c773ff06e8c35a839
vladsav1980 <vladsav1980@gmail.com>
2023-09-30 16:38:46 +0300

    gh-pages

--- index.js
+++ index.js
@@ -1,97 +1,153 @@
-#!/usr/bin/env node
+class Todo {
+  static #list = []
+  static #count = 0
+  static #NAME = 'todo'
 
-/**
- * Module dependencies.
- */
-
-const app = require('./app')
-const debug = require('debug')(
-  'template-express-live-reload:server',
-)
-const http = require('http')
-
-/**
- * Get port from environment and store in Express.
- */
-
-const port = normalizePort(process.env.PORT || '3000')
-app.set('port', port)
-
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app)
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port)
-server.on('error', onError)
-server.on('listening', onListening)
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  const port = parseInt(val, 10)
-
-  if (isNaN(port)) {
-    // named pipe
-    return val
+  static #saveData = () => {
+    localStorage.setItem(
+      this.#NAME,
+      JSON.stringify({
+        list: this.#list,
+        count: this.#count,
+      }),
+    )
   }
 
-  if (port >= 0) {
-    // port number
-    return port
+  static #loadData = () => {
+    const data = localStorage.getItem(this.#NAME)
+
+    if (data) {
+      const { list, count } = JSON.parse(data)
+      this.#list = list
+      this.#count = count
+    }
   }
 
-  return false
+  static #createTaskData = (text) => {
+    this.#list.push({
+      id: ++this.#count,
+      text,
+      done: false,
+    })
+  }
+  static #block = null
+  static #template = null
+  static #input = null
+  static #button = null
+  static #clear = null
+
+  static init = () => {
+    this.#template =
+      document.getElementById(
+        'task',
+      ).content.firstElementChild
+
+    this.#block = document.querySelector('.task__list')
+    this.#input = document.querySelector('.form__input')
+    this.#button = document.querySelector('.form__button')
+    this.#clear = document.querySelector(
+      '.form__button--clear',
+    )
+
+    this.#button.onclick = this.#handleAdd
+    this.#input.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        this.#handleAdd()
+      }
+    })
+
+    this.#clear.onclick = this.#handleClear
+    this.#loadData()
+    this.#render()
+  }
+
+  static #handleAdd = () => {
+    const value = this.#input.value
+    if (value.length > 1) {
+      this.#createTaskData(value)
+      this.#input.value = ''
+      this.#render()
+      this.#saveData()
+    }
+  }
+
+  static #render = () => {
+    this.#block.innerHTML = ''
+
+    if (this.#list.length === 0) {
+      this.#block.innerText = 'The list is empty'
+    } else {
+      this.#list.forEach((taskData) => {
+        const el = this.#createTaskElem(taskData)
+        this.#block.append(el)
+      })
+    }
+  }
+
+  static #createTaskElem = (data) => {
+    const el = this.#template.cloneNode(true)
+
+    const [id, text, btnDo, btnCancel] = el.children
+
+    id.innerText = ''
+
+    text.innerText = data.text
+
+    text.onclick = () => this.#handleDo(data, btnDo, el)
+
+    btnDo.onclick = () => this.#handleDo(data, btnDo, el)
+
+    btnCancel.onclick = () => this.#handleCancel(data)
+
+    if (data.done) {
+      el.classList.add('task--done')
+      btnDo.classList.add('task__button--done')
+      btnDo.classList.remove('task__button--do')
+    }
+    return el
+  }
+
+  static #handleDo = (data, btn, el) => {
+    const result = this.#toggleDone(data.id)
+    if (result === true || result === false) {
+      el.classList.toggle('task--done')
+      btn.classList.toggle('task__button--done')
+      btn.classList.toggle('task__button--do')
+      this.#saveData()
+    }
+  }
+
+  static #toggleDone = (id) => {
+    const task = this.#list.find((item) => item.id === id)
+    if (task) {
+      task.done = !task.done
+      return task.done
+    } else {
+      return null
+    }
+  }
+
+  static #handleCancel = (data) => {
+    const result = this.#deleteById(data.id)
+    if (result) {
+      this.#render()
+      this.#saveData()
+    }
+  }
+
+  static #deleteById = (id) => {
+    this.#list = this.#list.filter((item) => item.id !== id)
+    return true
+  }
+
+  static #handleClear = () => {
+    this.#list = []
+    this.#count = 0
+    localStorage.removeItem(this.#NAME)
+    this.#render()
+  }
 }
 
-/**
- * Event listener for HTTP server "error" event.
- */
+Todo.init()
 
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error
-  }
-
-  const bind =
-    typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.log(bind + ' requires elevated privileges')
-      process.exit(1)
-      break
-    case 'EADDRINUSE':
-      console.log(bind + ' is already in use')
-      process.exit(1)
-      break
-    default:
-      throw error
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  const addr = server.address()
-  const bind =
-    typeof addr === 'string'
-      ? 'pipe ' + addr
-      : 'port ' + addr.port
-  debug('Listening on ' + bind)
-  console.log(
-    'Listening on ' + 'http://localhost:' + addr.port,
-  )
-}
+window.todo = Todo
